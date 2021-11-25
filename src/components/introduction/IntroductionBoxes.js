35f2fd97ad217a1eb9edfda645e65d9735ba08f9
santi vittor <santi.vittor-@live.com.ar>
2021-11-25 13:57:44 -0300

    0.1.7 Fix: test para solucionar problemas de compatibilidad con iOS

--- src/components/introduction/IntroductionBoxes.js
+++ src/components/introduction/IntroductionBoxes.js
@@ -19,32 +19,30 @@ const IntroductionBoxes = () => {
         <div className="introductionContainer__boxesContainer" >
                 <div className="introductionContainer__boxesContainer--box" data-aos="flip-left" data-aos-duration="2200">
                     <img src={marketing} alt="img1"/>
-                    <h1>
+                    <h2>
                     MARKETING ONLINE
-                    </h1>
+                    </h2>
                     <p>
                     Atraemos personas interesadas a tu negocio por medio de campañas publicitarias en las plataformas de Google, Facebook e Instagram.
                     </p>
                 </div>
                 <div className="introductionContainer__boxesContainer--box" data-aos="flip-left" data-aos-duration="1700" > 
                     <img src={webDevices} alt="img2"/>
-                    <h1>
+                    <h2>
                     DISEÑO WEB
-                    </h1>
+                    </h2>
                     <p>
                     Diseñamos sitios web para empresas y PyMEs que buscan mejorar su imagen en Internet e incrementar la interacción de sus clientes con su marca.
-
                     </p>
                 </div>
 
                 <div className="introductionContainer__boxesContainer--box" data-aos="flip-left" data-aos-duration="1200" >
                     <img src={eCommerce} alt="img3"/>
-                    <h1>
+                    <h2>
                     E-COMMERCE
-                    </h1>
+                    </h2>
                     <p>
                     Creamos y administramos sistemas de ventas online para empresas o PyME que desean comercializar sus productos en Internet y expandir su negocio.
-
                     </p>
                 </div>
 
